1de9cbbe0e4511c06af24f5e32623f8a3c71d2d6
CarlosBCIII <henrique.alves@prf.gov.br>
2023-02-11 18:35:00 -0300

    arqs para execução API

---
+++ employee-api/server.js
@@ -0,0 +1,8 @@
+/**
+ * Responsável por toda configuração e execução do back
+ */
+const app = require('./src/app');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log('Aplicação sendo executada...', port)
+})
---
+++ employee-api/src/app.js
@@ -0,0 +1,20 @@
+ /**
+  * Responsável pela conxão com server.js
+  */
+ const express = require('express'); //conecta com express e cors
+ const cors = require('cors');
+
+ const app = express();
+ const index = require('./routes/index');
+ // const empregadosRotas = require('./routes/empregados.routes');
+
+ app.use(express.urlencoded({ extended: true }))
+ app.use(express.json())
+ app.use(express.json({ type: 'application/vnd.api+json' }))
+ app.use(cors())
+
+
+ app.use(index)
+     //app.use('/api/', empregadosRotas)
+
+ module.exports = app
