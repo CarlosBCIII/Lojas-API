46b82899ba5c489208f9560685958633e7bfa7dd
CarlosBCIII <henrique.alves@prf.gov.br>
2023-02-13 14:51:32 -0300

    Desenvolvimentos das totas update

--- employee-api/src/routes/empregados.routes.js
+++ employee-api/src/routes/empregados.routes.js
@@ -4,10 +4,27 @@ const empregadosController = require('../controllers/empregados.controller')
 
 //==>Rota para criar colaborador'Colaborador':(POST):localhost:3000/api/employees
 
-router.post('/employees', empregadosController.createEmpregado)
+router.post('/employees', empregadosController.createEmpregados)
 
 // Rota listr todos colaboradores :(GET):localhost:3000/api/employees
 
-router.get('/employees', empregadosController.listaEmpregado)
+router.get('/employees', empregadosController.listaEmpregados)
+
+// Rota listr colaborador por IDs :(GET):localhost:3000/api/employees/id/
+
+router.get('/employees/id/:codigo', empregadosController.listaEmpregadosID)
+
+// Rota listr colaborador por Registro :(GET):localhost:3000/api/employees/registro/
+
+router.get('/employees/registro/:registro', empregadosController.listaEmpregadosRegistro)
+
+// Rota listr colaborador por Registro :(GET):localhost:3000/api/employees/nome/
+
+router.get('/employees/nome/:nome', empregadosController.listaEmpregadosNome)
+
+// Rota atualizar por ID :(PUT):localhost:3000/api/employees/id/
+
+router.put('/employees/id/:codigo', empregadosController.atualizaEmpregadosCodigo)
+
 
 module.exports = router
